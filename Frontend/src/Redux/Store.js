a5ddaf2084a94b4176381ca0b7b3cb58a6da2e7c
RussellAhmed11 <you@example.comahmedrussell437@gmail.com>
2025-09-27 02:08:44 +0600

    cart,checkout,admin reducer create

--- Frontend/src/Redux/Store.js
+++ Frontend/src/Redux/Store.js
@@ -1,10 +1,18 @@
 import {configureStore} from "@reduxjs/toolkit"
 import authReducer from "./Slices/AuthSlices"
 import productReducer from "./Slices/ProductSlices"
+import cartReducer from "./Slices/CartSlices"
+import checkoutReducer from "./Slices/checkoutSlices"
+import orderReducer from "./Slices/OrderSlices"
+import adminReducer from "./Slices/adminSlices"
 const store=configureStore({
     reducer:{
         auth:authReducer,
         products:productReducer,
+        cart:cartReducer,
+        checkout:checkoutReducer,
+        orders:orderReducer,
+        admin:adminReducer,
     }
 })
 
